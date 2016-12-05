363cf6067b82ea2ab0b4f6b0c9d087c725683919
Chris Aumann <me@chr4.org>
2016-12-05 23:26:45 +0100

    Implement repl [01]

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,27 @@
-fn main() {}
+use std::io::{self, BufRead, Write};
+
+pub mod lexer;
+pub mod token;
+
+// Start a custom repl
+fn main() {
+    let stdin = io::stdin();
+
+    loop {
+        // Stdout needs to be flushed, due to missing newline
+        print!(">> ");
+        io::stdout().flush().expect("Error flushing stdout");
+
+        let mut line = String::new();
+        stdin.lock().read_line(&mut line).expect("Error reading from stdin");
+        let mut lexer = lexer::Lexer::new(&mut line);
+
+        loop {
+            let tok = lexer.next_token();
+            println!("{:?}", tok);
+            if tok.token_type == token::EOF {
+                break;
+            }
+        }
+    }
+}
